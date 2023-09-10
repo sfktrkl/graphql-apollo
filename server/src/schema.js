695d43d73e9666a74d6b44523ac156526d5b8cf1
Safak Turkeli <sfktrkl@gmail.com>
2023-09-11 00:02:37 +0300

    Adding a mutation to our schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -7,6 +7,19 @@ const typeDefs = gql`
     "Get a specific track, provided a track's ID"
     track(id: ID!): Track
   }
+  type Mutation {
+    "Increment the track's number of views, provided a track's ID"
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse
+  }
+  type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    track: Track
+  }
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
     id: ID!
