234ac76809b13025736f49684c85c787114b50e9
Safak Turkeli <sfktrkl@gmail.com>
2023-09-11 02:21:18 +0300

    Fetch and display the modules

--- client/src/pages/module.js
+++ client/src/pages/module.js
@@ -1,10 +1,41 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
 import { Layout, QueryResult } from "../components";
+import ModuleDetail from "../components/module-detail";
 import { useParams } from "react-router-dom";
+
+const GET_MODULE_AND_PARENT_TRACK = gql`
+  query getModuleAndParentTrack($moduleId: ID!, $trackId: ID!) {
+    module(id: $moduleId) {
+      id
+      title
+      content
+      videoUrl
+    }
+    track(id: $trackId) {
+      id
+      title
+      modules {
+        id
+        title
+        length
+      }
+    }
+  }
+`;
 
 const Module = () => {
   const { trackId, moduleId } = useParams();
-  return <Layout fullWidth={true}></Layout>;
+  const { loading, error, data } = useQuery(GET_MODULE_AND_PARENT_TRACK, {
+    variables: { moduleId, trackId },
+  });
+  return (
+    <Layout>
+      <QueryResult error={error} loading={loading} data={data}>
+        <ModuleDetail track={data?.track} module={data?.module} />
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Module;
