7f98f250e83736addb11a4c6bc6110723091122d
Safak Turkeli <sfktrkl@gmail.com>
2023-09-10 23:10:20 +0300

    Resolver chains

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -14,6 +14,10 @@ class TrackAPI extends RESTDataSource {
   getTrack(trackId) {
     return this.get(`track/${trackId}`);
   }
+
+  getTrackModules(trackId) {
+    return this.get(`track/${trackId}/modules`);
+  }
 }
 
 module.exports = TrackAPI;
--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -31,6 +31,9 @@ const resolvers = {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
+    modules: ({ id }, _, { dataSources }) => {
+      return dataSources.trackAPI.getTrackModules(id);
+    },
   },
 };
 
