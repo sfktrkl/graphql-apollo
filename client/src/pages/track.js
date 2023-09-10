3154baf18a68452193c4a38312710e46402f62c8
Safak Turkeli <sfktrkl@gmail.com>
2023-09-10 23:37:35 +0300

    Building the track page

---
+++ client/src/pages/track.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import { Layout, QueryResult } from "../components";
+import { useParams } from "react-router-dom";
+
+const GET_TRACK = gql`
+  query getTrack($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+      numberOfViews
+      modules {
+        id
+        title
+        length
+      }
+      description
+    }
+  }
+`;
+
+const Track = () => {
+  const { trackId } = useParams();
+  return <Layout></Layout>;
+};
+
+export default Track;
